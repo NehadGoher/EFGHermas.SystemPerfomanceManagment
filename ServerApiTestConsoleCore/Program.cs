c539a0fa115c4382fd271a337901b88b8814861a
NehadGoher <nehad.goher@gmail.com>
2019-06-23 13:51:31 +0200

    Heart Beat

--- ServerApiTestConsoleCore/Program.cs
+++ ServerApiTestConsoleCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using EFGHermes.SystemPerfomanceManagment.ServerAPI.Services;
 using System;
+using System.Threading;
 
 namespace ServerApiTestConsoleCore
 {
@@ -7,13 +8,16 @@ namespace ServerApiTestConsoleCore
     {
         static void Main(string[] args)
         {
+
+            // var timer1 = new Timer(_ => Console.WriteLine("Hello World"), null, 0, 2000);
             Agent agent = new Agent("https://localhost:44396/api/services");
             var services = agent.GetServicesAsync().Result;
             foreach (var elem in services)
             {
                 Console.WriteLine(elem.DisplayName);
-                
+
             }
+            Console.Read();
         }
     }
 }
