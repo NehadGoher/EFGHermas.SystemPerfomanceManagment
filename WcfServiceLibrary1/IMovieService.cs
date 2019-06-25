fd9d679420c233a3cd462bfbdc4a9f94adc0305e
hatem123 <hatemahly07@gmail.com>
2019-06-25 11:24:43 +0200

    add integrator projects

---
+++ WcfServiceLibrary1/IMovieService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMovieService
+    {
+        [OperationContract]
+        List<Movie> GetMovies();
+        [OperationContract]
+        Movie GetMovie(int id);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WcfServiceLibrary1.ContractType".
+    
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ WcfServiceLibrary1/MovieService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class MovieService : IMovieService
+    {
+        public List<Movie> Movies = new List<Movie>()
+        {
+            new Movie(){ Id=1,Name="Heba"},
+            new Movie(){ Id=1,Name="Amany"}
+        };
+        public Movie GetMovie(int id)
+        {
+            return Movies.Find((x) => x.Id == id);
+        }
+
+        public List<Movie> GetMovies()
+        {
+            return Movies;
+        }
+    }
+}
